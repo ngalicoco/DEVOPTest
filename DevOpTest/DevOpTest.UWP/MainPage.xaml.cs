e56d1a8ee0495b2e400f6e6d96ae48f67265da9d
ngali <ngali@co-construct.com>
2017-11-07 14:28:20 -0500

    intial commit master

---
+++ DevOpTest/DevOpTest.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DevOpTest.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new DevOpTest.App());
+        }
+    }
+}
---
+++ DevOpTest/DevOpTest/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DevOpTest
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
